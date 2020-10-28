84bdf854c59818bfbedf27d02511fac8cea38fca
Kino  Rose <kinoarose@gmail.com>
2020-10-27 21:10:19 -0400

    Fix all distribution and also integrate napkin into output

--- CleanJs.hx
+++ CleanJs.hx
@@ -46,31 +46,30 @@ class CleanJs {
 //=============================================================================\n';
       final filePath = '$distDir/$file';
       final contents = File.read(filePath).readAll().toString();
-      final cleanContents = contents.split("\n").map((lineContent) -> {
-        if (lineContent.contains("+=")
-          || (lineContent.contains("if(") && !lineContent.contains("_$LTGlobals_$"))
-          || lineContent.contains("#haxeui")
-          || lineContent.contains("haxe_ui")
-          || lineContent.contains("return")
-          || lineContent.contains("super(")
-          || lineContent.contains("fragSrc")
-          && !lineContent.contains("_$LTGlobals_$")) {
-          return lineContent;
-        } else {
-          pipe( // Below Removes Semi Colons Per Lin
-            ~/(\*\/);/g.replace(lineContent, "$1"), ~/(==);/g.replace(_, "$1"), ~/;(\()/g.replace(_, "$1"), ~/;(\/*)/g.replace(_, "$1"), ~/(\/\/.+\s*);/g.replace(_, "$1"), // Below Removes New Line Characters from the output file
-            ~/(?<!")\\n(?!")/g.replace(_, "\n"), ~/(?<=")\\n(?!")/g.replace(_, "\n"), // Below Special Removal Of Characters by appending them with @ symbols
-            ~/@"|"@/gi.replace(_, ""), ~/@(.*)@/gi.replace(_, "$1"), (~/_\$LTGlobals_\$\.(?!_)/gi).replace(_, ""));
-        }
-      }).join("\n");
+      // final cleanContents = contents.split("\n").map((lineContent) -> {
+      //   if (lineContent.contains("+=")
+      //     || (lineContent.contains("if(") && !lineContent.contains("_$LTGlobals_$"))
+      //     || lineContent.contains("#haxeui")
+      //     || lineContent.contains("haxe_ui")
+      //     || lineContent.contains("return")
+      //     || lineContent.contains("super(")
+      //     || lineContent.contains("fragSrc")
+      //     && !lineContent.contains("_$LTGlobals_$")) {
+      //     return lineContent;
+      //   } else {
+      //     pipe( // Below Removes Semi Colons Per Lin
+      //       ~/(\*\/);/g.replace(lineContent, "$1"), ~/(==);/g.replace(_, "$1"), ~/;(\()/g.replace(_, "$1"), ~/;(\/*)/g.replace(_, "$1"), ~/(\/\/.+\s*);/g.replace(_, "$1"), // Below Removes New Line Characters from the output file
+      //       ~/(?<!")\\n(?!")/g.replace(_, "\n"), ~/(?<=")\\n(?!")/g.replace(_, "\n"), // Below Special Removal Of Characters by appending them with @ symbols
+      //       ~/@"|"@/gi.replace(_, ""), ~/@(.*)@/gi.replace(_, "$1"), (~/_\$LTGlobals_\$\.(?!_)/gi).replace(_, ""));
+      //   }
+      // }).join("\n");
 
-      final newContent = fileNameStr + buildDate + madeWithStr + attributionStr + "\n" + cleanContents;
+      // final newContent = fileNameStr + buildDate + madeWithStr + attributionStr + "\n" + cleanContents;
       // final newContent = cleanContents;
-      File.write(filePath).writeString(newContent);
+      // File.write(filePath).writeString(newContent);
       var gamePath: String = generatePluginGamePath();
-      trace(gamePath);
       if (gamePath.length > 0) {
-        File.write(generatePluginGamePath() + file).writeString(newContent);
+        File.write(generatePluginGamePath() + file).writeString(contents);
         trace("Wrote File to Game Path");
       }
       trace("Cleaned Output File: " + filePath);
---
+++ src/core/README.md
@@ -0,0 +1,3 @@
+
+# Core
+> Contains core functionality that will be used across the visual novel essentials to speed up development or will be used in the final plugin.
